de0f22d597e07b479ddac8318d798368655c0bf1
Pavel Markeloff <pmarkelo77@gmail.com>
2022-04-01 21:03:16 +0400

    Добавление и редактирование моделей-представления

---
+++ src/WpfApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.ViewModels.Base;
+
+namespace WpfApp.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+
+        private string _Title = "Учет пожаров в МЧС";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get { return _Title; }
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/WpfApp/ViewModels/ReportViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp.ViewModels.Base;
+
+namespace WpfApp.ViewModels
+{
+    internal class ReportViewModel : ViewModelBase
+    {
+        #region Заголовок окна
+
+        private string _Title = "Отчет о пожаре";
+
+        /// <summary>
+        /// Заголовок окна
+        /// </summary>
+        public string Title
+        {
+            get { return _Title; }
+            set => Set(ref _Title, value);
+        }
+
+        #endregion
+    }
+}
