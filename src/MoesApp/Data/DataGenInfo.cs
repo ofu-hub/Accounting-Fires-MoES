c0bcc02fa91cd8d1d37aa29701ab03b744f3465f
Pavel Markeloff <pmarkelo77@gmail.com>
2022-05-22 20:01:23 +0400

    Редактирование контекста для "GenInfo"

--- src/MoesApp/Data/DataGenInfo.cs
+++ src/MoesApp/Data/DataGenInfo.cs
@@ -10,6 +10,11 @@ namespace MoesApp.Data
 {
     internal static class DataGenInfo
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public static GenInfo? GetGenInfoById(int id)
         {
             using (DataBaseContext db = new DataBaseContext())
@@ -21,6 +26,72 @@ namespace MoesApp.Data
                 else
                     return geninfo;
             }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="orgCode"></param>
+        /// <param name="datefire"></param>
+        /// <param name="typesettCode"></param>
+        /// <param name="typefiresettCode"></param>
+        /// <returns></returns>
+        public static string CreateGenInfo(string orgCode, DateTime datefire, string typesettCode, string typefiresettCode)
+        {
+            string result = "Ошибка!";
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                var newGenInfo = new GenInfo
+                {
+                    OrgCode = orgCode,
+                    Datefire = DateOnly.Parse(datefire.ToString("d")),
+                    TypesettCode = typesettCode,
+                    TypefiresettCode = typefiresettCode
+                };
+                db.GenInfos.Add(newGenInfo);
+                db.SaveChanges();
+                result = "Ок!";
+            }
+            return result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="genInfo"></param>
+        /// <returns></returns>
+        public static string DeleteGenInfo(GenInfo genInfo)
+        {
+            string result = "Ошибка!";
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                db.GenInfos.Remove(genInfo);
+                db.SaveChanges();
+                result = "Ок!";
+            }
+            return result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="oldGenInfo"></param>
+        /// <param name="orgCode"></param>
+        /// <param name="datefire"></param>
+        /// <param name="typesettCode"></param>
+        /// <param name="typefiresettCode"></param>
+        /// <returns></returns>
+        public static string EditGenInfo(GenInfo oldGenInfo, string orgCode, DateTime datefire, string typesettCode, string typefiresettCode)
+        {
+            string result = "Ошибка!";
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                GenInfo newGenInfo = db.GenInfos.FirstOrDefault(f => f.Id == oldGenInfo.Id);
+                newGenInfo.OrgCode = orgCode;
+                newGenInfo.Datefire = DateOnly.Parse(datefire.ToString("d"));
+                newGenInfo.TypesettCode = typesettCode;
+                newGenInfo.TypefiresettCode = typefiresettCode;
+                db.SaveChanges();
+                result = "Ок!";
+            }
+            return result;
         }
     }
 }
