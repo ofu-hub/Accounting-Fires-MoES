4f4717d0e44281980f3b51a1169cf80f4ed07583
Pavel Markeloff <pmarkelo77@gmail.com>
2022-05-22 19:06:42 +0400

    Редактирование контекста для "FireForcesMean"

--- src/MoesApp/Data/DataFireForcesMean.cs
+++ src/MoesApp/Data/DataFireForcesMean.cs
@@ -10,6 +10,11 @@ namespace MoesApp.Data
 {
     internal static class DataFireForcesMean
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public static FireForcesMean? GetFireForcesMeanById(int id)
         {
             using (DataBaseContext db = new DataBaseContext())
@@ -21,6 +26,86 @@ namespace MoesApp.Data
                 else
                     return forefm;
             }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firefighters"></param>
+        /// <param name="technique"></param>
+        /// <param name="numTechnique"></param>
+        /// <param name="individProtect"></param>
+        /// <param name="waterOnFire"></param>
+        /// <param name="fireExtManager"></param>
+        /// <param name="fireFightingHead"></param>
+        /// <param name="investigativeTeam"></param>
+        /// <param name="fireExtAgents"></param>
+        /// <param name="primaryFireEquip"></param>
+        /// <returns></returns>
+        public static string CreateFireForcesMean(string firefighters, string technique, string numTechnique, 
+                                                    string individProtect, string waterOnFire, string fireExtManager,
+                                                    string fireFightingHead, string investigativeTeam, string fireExtAgents, string primaryFireEquip)
+        {
+            string result = "Ошибка!";
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                var newFireForcesMean = new FireForcesMean
+                {
+                    Firefighters = firefighters.Split(','),
+                    Technique = technique.Split(','),
+                    NumTechnique = numTechnique.Split(','),
+                    IndividProtect = individProtect.Split(','),
+                    WaterOnFire = waterOnFire.Split(','),
+                    FireExtManager = fireExtManager.Split(','),
+                    FireFightingHead = fireFightingHead.Split(','),
+                    InvestigativeTeam = investigativeTeam,
+                    FireExtAgents = fireExtAgents.Split(','),
+                    PrimaryFireEquip = primaryFireEquip.Split(','),
+                };
+                db.FireForcesMeans.Add(newFireForcesMean);
+                db.SaveChanges();
+                result = "Ок!";
+            }
+            return result;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="fireForcesMean"></param>
+        /// <returns></returns>
+        public static string DeleteFireForcesMean(FireForcesMean fireForcesMean)
+        {
+            string result = "Ошибка!";
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                db.FireForcesMeans.Remove(fireForcesMean);
+                db.SaveChanges();
+                result = "Ок!";
+            }
+            return result;
+        }
+
+        public static string EditFireForcesMean(FireForcesMean oldFireForcesMean, string firefighters, string technique, string numTechnique,
+                                                    string individProtect, string waterOnFire, string fireExtManager, string fireFightingHead, 
+                                                    string investigativeTeam, string fireExtAgents, string primaryFireEquip)
+        {
+            string result = "Ошибка!";
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                FireForcesMean newFireForcesMean = db.FireForcesMeans.FirstOrDefault(f => f.Id == oldFireForcesMean.Id);
+                newFireForcesMean.Firefighters = firefighters.Split(',');
+                newFireForcesMean.Technique = technique.Split(',');
+                newFireForcesMean.NumTechnique = numTechnique.Split(',');
+                newFireForcesMean.IndividProtect = individProtect.Split(',');
+                newFireForcesMean.WaterOnFire = waterOnFire.Split(',');
+                newFireForcesMean.FireExtManager = fireExtManager.Split(',');
+                newFireForcesMean.FireFightingHead = fireFightingHead.Split(',');
+                newFireForcesMean.InvestigativeTeam = investigativeTeam;
+                newFireForcesMean.FireExtAgents = fireExtAgents.Split(',');
+                newFireForcesMean.PrimaryFireEquip = primaryFireEquip.Split(',');
+                db.SaveChanges();
+                result = "Ок!";
+            }
+            return result;
         }
     }
 }
